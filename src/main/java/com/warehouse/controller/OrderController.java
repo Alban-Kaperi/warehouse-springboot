7165501659274a14e7a614c7984aef7efe859a6a
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-09 21:37:08 +0100

    add get all and get by id

---
+++ src/main/java/com/warehouse/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.warehouse.controller;
+
+import com.warehouse.enums.OrderStatus;
+import com.warehouse.model.Order;
+import com.warehouse.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Order>> findAllOrders() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> findOrderById(Long id) {
+        return orderService.findById(id) != null ? ResponseEntity.ok(orderService.findById(id)) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(Order order) {
+        order.setStatus(OrderStatus.CREATED);
+        return ResponseEntity.ok(orderService.create(order));
+    }
+
+}
