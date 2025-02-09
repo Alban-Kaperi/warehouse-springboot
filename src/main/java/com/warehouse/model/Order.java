1fc87911f0c57f5e0baf07eccc9a5d0072ae5dc6
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-09 15:50:49 +0100

    add relationships in the Order model with Item model and User model

--- src/main/java/com/warehouse/model/Order.java
+++ src/main/java/com/warehouse/model/Order.java
@@ -4,7 +4,9 @@ import com.warehouse.enums.OrderStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -27,6 +29,15 @@ public class Order {
     @Temporal(TemporalType.DATE)
     private String deadlineDate;
 
+    // One Order may have many items
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order", fetch = FetchType.LAZY)
+    private List<Item> items = new ArrayList<>();
+
+    // Many Orders can be created by one User
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+
     // Usually I would do it in the service Layer when I create the order, but because it was in the description of the project I did it like this.
     @PrePersist
     public void generateOrderNumberAndSubmittedDate() {
@@ -37,6 +48,8 @@ public class Order {
             this.submittedDate = new Date();
         }
     }
+
+    // Getters and Setters
 
     public Long getId() {
         return id;
@@ -77,4 +90,21 @@ public class Order {
     public void setDeadlineDate(String deadlineDate) {
         this.deadlineDate = deadlineDate;
     }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User client) {
+        this.user = client;
+    }
+
 }
