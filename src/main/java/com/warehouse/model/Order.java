ce082d24935047ee2030dbfbaf965d4ee6439cf9
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-09 14:18:58 +0100

    create Order model

---
+++ src/main/java/com/warehouse/model/Order.java
@@ -0,0 +1,65 @@
+package com.warehouse.model;
+
+import com.warehouse.enums.OrderStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Order {
+    // 	id, orderNumber, submittedDate, status, deadlineDate
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Order Number is required")
+    @Column(unique = true)
+    private String orderNumber;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private String submittedDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Temporal(TemporalType.DATE)
+    private String deadlineDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getSubmittedDate() {
+        return submittedDate;
+    }
+
+    public void setSubmittedDate(String submittedDate) {
+        this.submittedDate = submittedDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public String getDeadlineDate() {
+        return deadlineDate;
+    }
+
+    public void setDeadlineDate(String deadlineDate) {
+        this.deadlineDate = deadlineDate;
+    }
+}
