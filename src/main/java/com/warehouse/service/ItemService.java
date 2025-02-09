9fb09cea164f89598a3adb8039a2f928f4a7687b
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-09 18:08:29 +0100

    create a base ItemService

---
+++ src/main/java/com/warehouse/service/ItemService.java
@@ -0,0 +1,37 @@
+package com.warehouse.service;
+
+import com.warehouse.model.Item;
+import com.warehouse.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public Item create (Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item update(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item getItemById(Long id) {
+        return itemRepository.findById(id).orElse(null);
+    }
+
+    public void delete(Item item) {
+        itemRepository.delete(item);
+    }
+
+    public List<Item> getAll() {
+        return itemRepository.findAll();
+    }
+}
