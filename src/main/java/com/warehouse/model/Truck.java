0ebdb450c9715516b70abdcf9b8286301a23cfb3
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-09 18:18:26 +0100

    remove the Id column and make the chassisNumber as primary

--- src/main/java/com/warehouse/model/Truck.java
+++ src/main/java/com/warehouse/model/Truck.java
@@ -7,24 +7,14 @@ import jakarta.validation.constraints.NotEmpty;
 public class Truck {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotEmpty(message = "Truck Number is required")
-    @Column(unique = true)
+    @Column(unique=true)
     private String chassisNumber;
 
     @NotEmpty(message = "License Plate is required")
     @Column(unique = true)
     private String licensePlate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // Getters and Setters
 
     public String getChassisNumber() {
         return chassisNumber;
