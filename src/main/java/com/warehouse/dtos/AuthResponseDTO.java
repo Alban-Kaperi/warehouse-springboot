57be41406af1f5007fbbb0dea3ceee47c41ffca0
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-11 10:49:20 +0100

    create AuthRequestDTO

---
+++ src/main/java/com/warehouse/dtos/AuthResponseDTO.java
@@ -0,0 +1,19 @@
+package com.warehouse.dtos;
+
+public class AuthResponseDTO {
+    private String token;
+    private String type = "Bearer ";
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+}
