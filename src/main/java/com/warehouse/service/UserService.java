21214cb731ecd2b4994747e00c69ba16c69259d7
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-12 12:51:05 +0100

    modify the Service to handle more logic

--- src/main/java/com/warehouse/service/UserService.java
+++ src/main/java/com/warehouse/service/UserService.java
@@ -1,38 +1,50 @@
 package com.warehouse.service;
 
+import com.warehouse.dto.UserResponseDto;
+import com.warehouse.exception.UserNotFoundException;
+import com.warehouse.mapper.UserResponseMapper;
 import com.warehouse.model.User;
 import com.warehouse.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserResponseMapper userResponseMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserResponseMapper userResponseMapper) {
         this.userRepository = userRepository;
+        this.userResponseMapper = userResponseMapper;
     }
 
-    public User save(User user) {
-        return userRepository.save(user);
+    public List<UserResponseDto> findAll(){
+        return userRepository.findAll()
+                .stream()
+                .map(userResponseMapper::toDto)
+                .collect(Collectors.toList());
     }
 
-    public void delete(User user) {
+    public UserResponseDto saveUser(User user) {
+        return userResponseMapper.toDto(userRepository.save(user));
+    }
+
+    public void delete(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
         userRepository.delete(user);
-    }
-
-    public List<User> findAll() {
-        return userRepository.findAll();
     }
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
 
-    public User findById(Long id) {
-        return userRepository.findById(id).orElse(null);
+    public UserResponseDto findById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+        return userResponseMapper.toDto(user);
     }
 
 }
