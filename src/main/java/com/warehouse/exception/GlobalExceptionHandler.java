c9b85b4fbb373670653c30cf05007404c775b391
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-12 12:50:49 +0100

    create user Exception

--- src/main/java/com/warehouse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/warehouse/exception/GlobalExceptionHandler.java
@@ -18,6 +18,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
