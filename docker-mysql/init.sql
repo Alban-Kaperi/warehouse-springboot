346d66b940d2d51cfb6a846e9a97223d482ce903
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-13 00:15:13 +0100

    add some trucks in the sql statement

--- docker-mysql/init.sql
+++ docker-mysql/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS inventory;
 USE inventory;
 
--- Create 'roles' table if it doesn't exist
+-- Create roles table if it doesn't exist
 CREATE TABLE IF NOT EXISTS roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY, -- I know that I could have used also INT because roles table generally doesn't hold many records.
     name VARCHAR(100) UNIQUE NOT NULL
@@ -20,7 +20,7 @@ INSERT INTO roles (name)
 SELECT * FROM (SELECT 'SYSTEM_ADMIN') AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'SYSTEM_ADMIN');
 
--- Create the 'items' table if it doesn't exist
+-- Create the items table if it doesn't exist
 CREATE TABLE IF NOT EXISTS items (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -41,3 +41,23 @@ VALUES
     ON DUPLICATE KEY UPDATE
          quantity = VALUES(quantity),
          price = VALUES(price); -- Ensures no duplicate entries for items with the same name
+
+-- Create trucks table if it doesn't exist
+CREATE TABLE IF NOT EXISTS trucks (
+    chassis_number VARCHAR(100) PRIMARY KEY, -- Unique identifier for the truck
+    license_plate VARCHAR(100) UNIQUE NOT NULL -- Unique license plate for the truck
+);
+
+-- Insert sample data
+INSERT INTO trucks (chassis_number, license_plate)
+SELECT * FROM (SELECT 'CHASSIS001', 'ABC123') AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM trucks WHERE chassis_number = 'CHASSIS001' OR license_plate = 'ABC123');
+
+INSERT INTO trucks (chassis_number, license_plate)
+SELECT * FROM (SELECT 'CHASSIS002', 'XYZ789') AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM trucks WHERE chassis_number = 'CHASSIS002' OR license_plate = 'XYZ789');
+
+INSERT INTO trucks (chassis_number, license_plate)
+SELECT * FROM (SELECT 'CHASSIS003', 'LMN456') AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM trucks WHERE chassis_number = 'CHASSIS003' OR license_plate = 'LMN456');
+
