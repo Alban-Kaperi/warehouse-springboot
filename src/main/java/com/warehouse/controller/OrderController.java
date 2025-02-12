996a42d4e93b07972ee3448f9dffe1d6baae8ff9
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-13 00:17:29 +0100

    changing url to api/inventory/orders

--- src/main/java/com/warehouse/controller/OrderController.java
+++ src/main/java/com/warehouse/controller/OrderController.java
@@ -4,13 +4,14 @@ import com.warehouse.enums.OrderStatus;
 import com.warehouse.model.Order;
 import com.warehouse.service.OrderService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/inventory/orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -30,6 +31,7 @@ public class OrderController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<Order> createOrder(Order order) {
         order.setStatus(OrderStatus.CREATED);
         return ResponseEntity.ok(orderService.create(order));
