7ea7ef7a6f270d60b6340ecb7d34523fa638c159
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-12 12:54:14 +0100

    remove un-used imports

--- src/main/java/com/warehouse/controller/AdminController.java
+++ src/main/java/com/warehouse/controller/AdminController.java
@@ -1,20 +1,16 @@
 package com.warehouse.controller;
 
 import com.warehouse.dto.UserResponseDto;
-import com.warehouse.mapper.UserResponseMapper;
 import com.warehouse.model.User;
-import com.warehouse.repository.UserRepository;
+import com.warehouse.service.UserService;
+import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/admin/users")
@@ -23,30 +19,33 @@ public class AdminController {
 
     private static final Logger logger = LogManager.getLogger(AdminController.class);
 
-    private final UserRepository userRepository;
-    private final UserResponseMapper userResponseMapper;
+    private final UserService userService;
 
-    public AdminController(UserRepository userRepository, UserResponseMapper userResponseMapper) {
-        this.userRepository = userRepository;
-        this.userResponseMapper = userResponseMapper;
+    public AdminController( UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public List<UserResponseDto> getAllUsers() {
-        return userRepository.findAll()
-                .stream()
-                .map(userResponseMapper::toDto)
-                .collect(Collectors.toList());
+        return userService.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
-        User user = userRepository.findUserById(id);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(userResponseMapper.toDto(user));
+        return ResponseEntity.ok(userService.findById(id));
     }
 
+    @PostMapping
+    public ResponseEntity<UserResponseDto> saveUser(@Valid @RequestBody User user){
+        UserResponseDto savedUser = userService.saveUser(user);
+        logger.info("User with id: {} saved", user.getId());
+        return ResponseEntity.ok(savedUser);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.delete(id);
+        logger.info("User with id: {} deleted", id);
+        return ResponseEntity.noContent().build();
+    }
 }
