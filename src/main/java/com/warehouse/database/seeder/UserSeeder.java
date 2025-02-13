f8cf6a25aa61d0354d5a204b705bc937665bd158
Alban-Kaperi <albanikaperi@gmail.com>
2025-02-14 00:35:11 +0100

    separate roles to role seeder

--- src/main/java/com/warehouse/database/seeder/UserSeeder.java
+++ src/main/java/com/warehouse/database/seeder/UserSeeder.java
@@ -1,15 +1,18 @@
 package com.warehouse.database.seeder;
 
+import com.warehouse.enums.RoleEnum;
 import com.warehouse.model.Role;
 import com.warehouse.model.User;
 import com.warehouse.repository.RoleRepository;
 import com.warehouse.repository.UserRepository;
+import com.warehouse.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -25,8 +28,20 @@ public class UserSeeder implements CommandLineRunner {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private RoleService roleService;
+
     @Override
     public void run(String... args) throws Exception {
+
+        // Create Roles
+//        for (RoleEnum roleEnum : RoleEnum.values()) {
+//            roleRepository.findByName(roleEnum.name()).orElseGet(() -> {
+//                Role newRole = new Role();
+//                newRole.setName(roleEnum.name());
+//                return roleRepository.save(newRole);
+//            });
+//        }
 
         // Check if admin role exists
         Role adminRole = roleRepository.findByName("SYSTEM_ADMIN")
@@ -41,9 +56,11 @@ public class UserSeeder implements CommandLineRunner {
             User adminUser = new User();
             adminUser.setUsername("admin");
             adminUser.setPassword(passwordEncoder.encode("123456"));
-            adminUser.setRoles(Set.of(adminRole));
-
             userRepository.save(adminUser);
+
+            // Sync the role to the admin
+            roleService.syncUserRoles(adminUser.getId(), List.of(adminRole.getName()));
+
         }
 
     }
